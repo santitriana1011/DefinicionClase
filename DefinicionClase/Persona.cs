e97076fc4d0de775226644d729c01b1395e7c506
santitriana1011 <41800013+santitriana1011@users.noreply.github.com>
2019-04-30 17:57:04 -0500

    Actualizacion

---
+++ DefinicionClase/Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefinicionClase
+{
+   public class Persona
+    {
+        //Es para definir todas las propiedades
+        #region Propiedades
+        public int Edad { get; set; }
+        public string Nombre { get; set; }
+        #endregion
+        
+        public void IngresarPersona()
+        {
+            Console.WriteLine("Ingrese su nombre");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad");
+            Edad = Convert.ToInt32(Console.ReadLine());
+        }
+        //Para Listar las personas que se hicieron en el ciclo while hecho en Program
+        public void ListarPersonas(List<Persona> users)
+        {
+            foreach (var item in users)
+            {
+                Console.WriteLine($"Nombre: { item.Nombre} Edad: {item.Edad}");
+            }
+        }
+
+
+    }
+}
--- DefinicionClase/Program.cs
+++ DefinicionClase/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DefinicionClase
 {
@@ -6,7 +7,55 @@ namespace DefinicionClase
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                //Asi se instancia
+                //    Persona per = new Persona();
+                //    Persona per2 = new Persona();
+                //    Persona per3 = new Persona();
+                List<Persona> personas = new List<Persona>();
+                ////Fin de la Instancia
+                ////Se le dan las propiedades a la instancia
+                //    per.Nombre = "KJ";
+                //    per.Edad = 18;
+                //    //Para que nos muestre en pantalla
+                //    Console.WriteLine($"Nombre:{per.Nombre} Edad: {per.Edad}");
+                //    Console.WriteLine("Ingrese su nombre");
+                //    per2.Nombre = Console.ReadLine();
+                //    Console.WriteLine("Ingrese su edad");
+                //    per2.Edad = Convert.ToInt32(Console.ReadLine());
+                //    Console.WriteLine($"Nombre:{per2.Nombre} Edad: {per2.Edad}");
+                //    per3.IngresarPersona();
+                //    personas.Add(per);
+                //    personas.Add(per2);
+                //    personas.Add(per3);
+                //    //Para Recorrer la lista
+                //    foreach (var item in personas)
+                //    {
+                //        Console.WriteLine($"Nombre: { item.Nombre} Edad: {item.Edad}");
+                //    }
+                // Ejercicio Ingresar Personas que desee
+                Console.WriteLine("¿Desea Ingresar Personas? SI / NO");
+                var res = Console.ReadLine();
+                while (res == "s")
+                {
+                    Persona per4 = new Persona();
+                    per4.IngresarPersona();
+                    personas.Add(per4);
+                    Console.WriteLine("¿Desea Ingresar Personas? SI / NO");
+                    res = Console.ReadLine();
+                }
+                Persona per = new Persona();
+                per.ListarPersonas(personas);
+                Console.ReadLine();
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Ha ocurrido un error faltal. Solucionalo ya Bastardo");
+                Console.ReadLine();
+            }
         }
     }
 }
